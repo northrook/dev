0d5177428ffeae859da14d2756ff5d85378c290b
Martin <hello@northrook.com>
2025-05-17 12:07:44 +0100

    mend

---
+++ src/Attribute/TODO.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace _Dev\Attribute;
+
+use Attribute;
+
+/**
+ * Indicate this feature is experimental.
+ */
+#[Attribute( Attribute::TARGET_ALL )]
+final readonly class TODO
+{
+    public function __construct( public ?string $note ) {}
+}
